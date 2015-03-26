ebe74195154a7db6d8e52f6a3740640d899bf1c7
hansi <hansi@infosupport.com>
2015-03-26 14:07:08 +0100

    init

---
+++ ClipboardManager/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ClipboardManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbStrings = new System.Windows.Forms.ListBox();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbStrings
+            // 
+            this.lbStrings.FormattingEnabled = true;
+            this.lbStrings.Location = new System.Drawing.Point(13, 13);
+            this.lbStrings.Name = "lbStrings";
+            this.lbStrings.Size = new System.Drawing.Size(309, 290);
+            this.lbStrings.TabIndex = 0;
+            this.lbStrings.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbStrings_MouseDoubleClick);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 310);
+            this.Controls.Add(this.lbStrings);
+            this.Name = "Form1";
+            this.Text = "Ytrog\'s Clipboard Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbStrings;
+        private System.Windows.Forms.Timer timerMain;
+    }
+}
+
---
+++ ClipboardManager/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClipboardManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int _maxItems = 5;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerMain.Enabled = true;
+        }
+
+        private void Poll()
+        {
+            string text = GetClipboardText();
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                lbStrings.Items.Insert(0,text);
+                Prune();
+            }
+        }
+
+        private void Prune()
+        {
+            if (lbStrings.Items.Count > _maxItems)
+            {
+                lbStrings.Items.RemoveAt(lbStrings.Items.Count - 1);
+            }
+        }
+
+        /// <summary>
+        /// Gets the latest entry from the clipboard
+        /// </summary>
+        /// <returns></returns>
+        private string GetClipboardText()
+        {
+            if  (!Clipboard.ContainsText() || (!string.IsNullOrWhiteSpace(LastEntry) && ItemsContainClipboard()))
+            {
+                return null;
+            }
+            return Clipboard.GetText();
+        }
+
+        private string LastEntry { get {return lbStrings.Items.Count > 0 ? lbStrings.Items[0] as string: null;} }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            Poll();
+        }
+
+        private void lbStrings_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Copy();
+        }
+
+        private bool ItemsContainClipboard()
+        {
+            string text = Clipboard.GetText();
+            return lbStrings.Items.Contains(text);
+        }
+
+        private void Copy()
+        {
+            string text = lbStrings.SelectedItem as string;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                Clipboard.SetText(text);
+            }
+        }
+    }
+}
